4ae545c538aa029ecbe66288ea23a495bf617d3f
Alex Vizcaino <avizcaino@uxland.es>
2018-07-31 12:34:22 +0200

    scripts

--- gulp/tasks/dist.js
+++ gulp/tasks/dist.js
@@ -1,25 +1,25 @@
-const gulp = require('gulp');
-const run = require('gulp-run');
-const moduleName = 'uxl-utilities';
-const path = require('path');
-const nodeModules = 'node_modules';
+const gulp = require("gulp");
+const run = require("gulp-run");
+const moduleName = "uxl-utilities";
+const path = require("path");
+const nodeModules = "node_modules";
 
-gulp.task('generate-d-ts', () =>{
-    require('dts-generator').default({
+gulp.task("generate-d-ts", () => {
+    require("dts-generator").default({
         name: moduleName,
-        out: './index.d.ts',
-        project: './gulp/tsconfig-dist.json',
-        baseDir: './src',
-        resolveModuleId: params =>{
+        out: "./index.d.ts",
+        project: "./gulp/tsconfig-dist.json",
+        baseDir: "./src",
+        resolveModuleId: params => {
             console.log(params.currentModuleId);
-            return params.currentModuleId === 'index' ? moduleName : `${moduleName}/${params.currentModuleId}`;
+            return params.currentModuleId === "index" ? moduleName : `${moduleName}/${params.currentModuleId}`;
         }
     });
 });
-gulp.task('dist', () =>{
-    const dir = path.basename(path.join(__dirname, '../../../'));
-    if(String(dir) == nodeModules){
-        return run('npm run dist').exec();
+gulp.task("dist", () => {
+    const dir = path.basename(path.join(__dirname, "../../../"));
+    if (String(dir) == nodeModules) {
+        return run("npm run dist").exec();
     }
-    console.log('skipped')
+    console.log("skipped");
 });
