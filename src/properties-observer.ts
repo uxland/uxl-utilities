18cc1211f08be6023f1ae5b54b25f7afc06879e7
dpinart <dpinart@newuibcn.com>
2018-08-21 13:25:50 +0200

    fix: collect tests added

--- src/properties-observer.ts
+++ src/properties-observer.ts
@@ -1,15 +1,11 @@
 import { dedupingMixin } from "@polymer/polymer/lib/utils/mixin";
-import {PropertiesMixin} from "@polymer/polymer/lib/mixins/properties-mixin";
-import {collect} from "./collect";
+import {PropertiesMixin, PropertiesMixinConstructor} from "@polymer/polymer/lib/mixins/properties-mixin";
 export const propertiesObserver = dedupingMixin(parent =>{
    class mixin extends PropertiesMixin(parent){
-       _initializeProperties(){
-           super._initializeProperties();
-       }
        _shouldPropertiesChange(currentProps: any, changedProps: any, oldProps: any){
            Object.keys(changedProps).filter(k => this[`${k}Changed`]).forEach(k => this[`${k}Changed`](changedProps[k], oldProps[k]));
            return true;
        }
    }
-   return (<any>mixin) as PropertiesMixin;
+   return (<any>mixin) as PropertiesMixinConstructor;
 });
