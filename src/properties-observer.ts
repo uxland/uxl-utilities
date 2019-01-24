bc63ad75c4748025d102bcf005f05dce7133038f
dpinart <dpinart@newuibcn.com>
2019-01-24 17:02:34 +0100

    perf: upgrade to lit-element rc
    BREAKING CHANGE: upgrade to lit-element rc

---
+++ src/properties-observer.ts
@@ -0,0 +1,25 @@
+import { dedupingMixin } from "@polymer/polymer/lib/utils/mixin";
+import {notEqual, PropertyDeclaration} from "lit-element";
+
+
+export const propertiesObserver = dedupingMixin(parent =>{
+    class mixin extends parent{
+
+        _requestPropertyUpdate(name: string, oldValue : any, options?: PropertyDeclaration){
+            if(this[`${name}Changed`]){
+                let current = this[name];
+                let comparer = (options ? options.hasChanged : notEqual) || notEqual;
+                if(comparer(current, oldValue))
+                    this[`${name}Changed`](current, oldValue);
+            }
+            if(super._requestPropertyUpdate)
+                super._requestPropertyUpdate(name, oldValue, options);
+        }
+        requestUpdate(properties: {name:string, old: any}[] = []){
+            if(super.requestUpdate)
+                return super.requestUpdate();
+            properties.forEach(p => this._requestPropertyUpdate(p.name, p.old));
+        }
+    }
+    return (<any>mixin);
+});
