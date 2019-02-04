44089f4659b1715eac1be203dd233ce2e825fd61
dpinart <dpinart@newuibcn.com>
2019-02-04 12:57:42 +0100

    fix: new dist

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 export * from './collect';
 export * from './constant-builder';
-export * from './import-href';
 export * from './invariant';
 export * from './listen';
 export * from './properties-observer';
