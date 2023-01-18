26691603b61fdceeb1e78fcf452f3b24042f3c0c
DanielCabiscol <danielcabiscol@gmail.com>
2023-01-18 08:19:39 +0100

    fix: avoid re-update 2 times in properties observer due a change in lit2

--- src/properties-observer.ts
+++ src/properties-observer.ts
@@ -1,18 +1,18 @@
-import { notEqual } from "lit";
-import defaultTo from "ramda/es/defaultTo";
-import pipe from "ramda/es/pipe";
-import { Constructor, dedupingMixin } from "./deduping-mixin";
-import { PropertiesObserverMixinFunction } from "./types";
+import {notEqual} from 'lit';
+import defaultTo from 'ramda/es/defaultTo';
+import pipe from 'ramda/es/pipe';
+import {Constructor, dedupingMixin} from './deduping-mixin';
+import {PropertiesObserverMixinFunction} from './types';
 
 const getPropertyComponentComparer = (name: PropertyKey, component: any) =>
   component.constructor._classProperties
     ? component.constructor._classProperties.get(name).hasChanged
     : undefined;
 const comparer = pipe(getPropertyComponentComparer, defaultTo(notEqual));
-export const propertiesObserver: PropertiesObserverMixinFunction =
-  dedupingMixin((superClass: Constructor<any>) => {
+export const propertiesObserver: PropertiesObserverMixinFunction = dedupingMixin(
+  (superClass: Constructor<any>) => {
     class PropertiesObserverMixin extends superClass {
-      updated(changedProperties) {
+      /* updated(changedProperties) {
         super.updated(changedProperties);
         changedProperties.forEach((oldValue, propName) => {
           if (comparer(propName, this)(this[propName], oldValue)) {
@@ -20,18 +20,18 @@ export const propertiesObserver: PropertiesObserverMixinFunction =
               this[`${String(propName)}Changed`](this[propName], oldValue);
           }
         });
-      }
+      } */
       requestUpdate(name, oldValue) {
         let result = super.requestUpdate
           ? super.requestUpdate(name, oldValue)
           : Promise.resolve(null);
         if (comparer(name, this)(this[name], oldValue)) {
-          if (this[`${String(name)}Changed`])
-            this[`${String(name)}Changed`](this[name], oldValue);
+          if (this[`${String(name)}Changed`]) this[`${String(name)}Changed`](this[name], oldValue);
         }
 
         return result;
       }
     }
     return PropertiesObserverMixin;
-  });
+  }
+);
